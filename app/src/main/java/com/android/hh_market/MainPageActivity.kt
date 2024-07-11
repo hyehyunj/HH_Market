59355118815832622b86a0ad4ec41a34e2f8ee94
hyehyunj <developh620@gmail.com>
2024-07-11 18:17:10 +0900

    feat: viewbinding, 상단 툴바 제거

--- app/src/main/java/com/android/hh_market/MainPageActivity.kt
+++ app/src/main/java/com/android/hh_market/MainPageActivity.kt
@@ -5,16 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.hh_market.databinding.ActivityMainPageBinding
 
 class MainPageActivity : AppCompatActivity() {
+
+
+    private lateinit var binding : ActivityMainPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainPageBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
     }
 }
